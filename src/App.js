a5d25d4d84d9116ce73d1b777a405ace4d940218
rajatsinghchauhan <singhrajatmyworld@gmail.com>
2021-11-16 18:37:35 +0530

    MODIFIED APP.JS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Qna from './Components/Qna';
-import Question_page from './Components/Question_page';
-import GiveAnswer from './Components/GiveAnswer';
-import Comments_for_an_answer from './Components/Comments_for_an_answer';
-import Write_a_comment from './Components/Write_a_comment';
-import Opportunities from './Components/Opportunities';
-import Blogs from './Components/Blogs';
-import AboutUs from './Components/AboutUs';
-import Profile from './Components/Profile';
+import Navbar from "./Components/Navbar";
+import Qna from "./Components/Qna";
+import Question_page from "./Components/Question_page";
+import GiveAnswer from "./Components/GiveAnswer";
+import Comments_for_an_answer from "./Components/Comments_for_an_answer";
+import Write_a_comment from "./Components/Write_a_comment";
+import Opportunities from "./Components/Opportunities";
+import Blogs from "./Components/Blogs";
+import AboutUs from "./Components/AboutUs";
+import Profile from "./Components/Profile";
+import Viewblog from "./Components/viewblog";
+import WriteACommentOnBlog from "./Components/writeacommentonblog";
+import Blogcomments from "./Components/blogcomments";
+import Newblog from "./Components/writeanewblog";
 //import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -20,15 +24,37 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route exact path="/"  element={ <Qna />} />           
+          <Route exact path="/" element={<Qna />} />
           <Route exact path="/about" element={<AboutUs />} />
           <Route exact path="/blogs" element={<Blogs />} />
-          <Route exact path="/opportunities" element={ <Opportunities />} />
-          <Route exact path="/question" element={ <Question_page/>} />
-          <Route exact path="/giveanswer" element={ <GiveAnswer/>} />
-          <Route exact path="/question/postComment" element={ <Question_page postComment={<Write_a_comment/>}/>} />
-          <Route exact path="/question/viewComments" element={ <Question_page viewComments={<Comments_for_an_answer/>} />}/>
-          <Route exact path="/myprofile" element={<Profile/>}/>
+          <Route exact path="/viewblog" element={<Viewblog />} />
+          <Route
+            exact
+            path="/viewblog/writeacommentonblog"
+            element={<Viewblog writeacommentonblog={<WriteACommentOnBlog />} />}
+          />
+          <Route
+            exact
+            path="/viewblog/blogcomments"
+            element={<Viewblog blogcomments={<Blogcomments />} />}
+          />
+          <Route exact path="/writeanewblog" element={<Newblog />} />
+          <Route exact path="/opportunities" element={<Opportunities />} />
+          <Route exact path="/question" element={<Question_page />} />
+          <Route exact path="/giveanswer" element={<GiveAnswer />} />
+          <Route
+            exact
+            path="/question/postComment"
+            element={<Question_page postComment={<Write_a_comment />} />}
+          />
+          <Route
+            exact
+            path="/question/viewComments"
+            element={
+              <Question_page viewComments={<Comments_for_an_answer />} />
+            }
+          />
+          <Route exact path="/myprofile" element={<Profile />} />
         </Routes>
       </Router>
       {/* <Qna /> */}
@@ -41,4 +67,3 @@ function App() {
 }
 
 export default App;
-
